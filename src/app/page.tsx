516a3298ae6b0c5463faf0819f48d47091f89dd7
Yavor Radulov <raduloov@gmail.com>
2024-11-11 14:33:01 +0200

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/83SbnXkuEV7wjR7PjURcWcyqdo14ZA5fhXEnzMjUI0YKGOep",
+  "https://utfs.io/f/83SbnXkuEV7wCZYLsjoJAz8TOKpy04mWVjuQfPSM32ZJvFgH",
+  "https://utfs.io/f/83SbnXkuEV7wQJpB3pYUd60PH5mcrbFDJyoBMLWGnYaf3Tgi",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello gallery
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
